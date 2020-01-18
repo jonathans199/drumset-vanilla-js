92d7e71eb547303aa28ba5f2747edba9f398cba3
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2020-01-18 14:03:17 -0500

    configured all sounds with keys properly

---
+++ script.js
@@ -0,0 +1,25 @@
+function playAudio(e) {
+  const audioElement = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+
+  // get keycode from event with "key" class
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+
+  if (!audioElement) return // stop function if keyCode pressed is not found
+  audioElement.currentTime = 0 // reset playing time to 0, so that it plays instantly
+
+  audioElement.play() // play the audio
+
+  key.classList.add('playing') // add the "playing" class
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return // end if Transform property name its not found
+
+  this.classList.remove('playing') // remove the playing class
+}
+
+const keys = document.querySelectorAll('.key') //
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+window.addEventListener('keydown', playAudio) // play the audio
